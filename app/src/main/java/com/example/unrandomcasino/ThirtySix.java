43ab1cd6f4da457d52040f6236a89a29303c1ba7
jdebilde <jean-jacques.debilde@ulb.be>
2020-12-20 21:09:04 +0100

    Implementation ThirtySix: bug when the game is finished!!

--- app/src/main/java/com/example/unrandomcasino/ThirtySix.java
+++ app/src/main/java/com/example/unrandomcasino/ThirtySix.java
@@ -11,7 +11,8 @@ import android.widget.TextView;
 
 public class ThirtySix extends AppCompatActivity {
     Integer currBet, _pot, _turn, _score, _dice;
-    String myTurn = "your turn", oppTurn = "opponent turn";
+    String myTurn = "your turn", oppTurn = "IA turn";
+    String _ia_bet = "IA bet %d $", _player_bet = "You bet %d  $", _ia_plays = "IA played %d", _player_plays = "You played %d", _ia_wins = "IA won", _player_wins = "you won";
     Boolean firsThrow = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,19 +22,23 @@ public class ThirtySix extends AppCompatActivity {
         TextView text_pot = findViewById(R.id.text_pot);
         TextView text_turn = findViewById(R.id.text_turn);
         TextView text_score = findViewById(R.id.text_score);
+        TextView text_dice = findViewById(R.id.text_dice);
+        TextView information = findViewById(R.id.information);
         Button throw_dice = findViewById(R.id.throw_dice);
 
         // money and bet (lucas)
         EditText bet = findViewById(R.id.bet);
         TextView currentMoney = findViewById(R.id.money2);
 
-
         currentMoney.setText(String.valueOf(Perso.getMoney()));
-        initTest();
+        resetGame();
         String p = _pot + "$";
         text_pot.setText(p);
         text_turn.setText(myTurn);
         text_score.setText(String.valueOf(_score));
+        text_dice.setText(String.valueOf(0));
+        information.setText(String.format(_ia_bet,50));
+
         throw_dice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -52,6 +57,7 @@ public class ThirtySix extends AppCompatActivity {
                             bet.setText(String.valueOf(Perso.getMoney()));
                         }
                         _pot += currBet;
+                        information.setText(String.format(_player_bet, currBet));
                         String p = _pot + "$";
                         text_pot.setText(p);
                         System.out.println("Le pot est de " + _pot);
@@ -61,32 +67,43 @@ public class ThirtySix extends AppCompatActivity {
                         int randNum = Perso.getSelectedRNG().getNumber(6);
                         System.out.println("vous jouez " + (randNum+1));
                         _score += (randNum+1);
-                        updateScreen(text_pot, text_turn, text_score);
-                        opponentPlay();
-                        updateScreen(text_pot, text_turn, text_score);
+                        _dice = (randNum+1);
+                        information.setText(String.format(_player_plays, _dice));
+                        
+                        checkWin();
+                        updateScreen(text_pot, text_turn, text_score, text_dice);
+                        throw_dice.setText("Next turn");
                     }
                 }
                 else {
-                    int randNum = Perso.getSelectedRNG().getNumber(6);
-                    _score += (randNum+1);
-                    System.out.println("vous jouez " + (randNum+1));
-                    updateScreen(text_pot, text_turn, text_score);
+                    if (_turn == 0) {
+                        int randNum = Perso.getSelectedRNG().getNumber(6);
+                        _score += (randNum+1);
+                        _dice = (randNum+1);
+                        System.out.println("vous jouez " + (randNum+1));
+                        information.setText(String.format(_player_plays, _dice));
+                        throw_dice.setText("Next turn");
+                    }
+                    else {
+                        opponentPlay();
+                        information.setText(String.format(_ia_plays, _dice));
+                        throw_dice.setText("Throw dice");
+                    }
+                    updateScreen(text_pot, text_turn, text_score, text_dice);
                     checkWin();
-                    updateScreen(text_pot, text_turn, text_score);
-                    opponentPlay();
-                    updateScreen(text_pot, text_turn, text_score);
-                    checkWin();
-                    updateScreen(text_pot, text_turn, text_score);
+                    updateScreen(text_pot, text_turn, text_score, text_dice);
                 }
             }
         });
 
     } // end onCreate
 
-    void initTest(){
+    void resetGame(){
+        _turn = 0;
+        firsThrow = true;
         _score = 0;
         _pot = 50;
-        _turn = 0;
+        _dice = 0;
     };
 
     void checkWin() {
@@ -98,10 +115,7 @@ public class ThirtySix extends AppCompatActivity {
                 System.out.println("Vous avez gagné");
                 Perso.setMoney(Perso.getMoney() + _pot);
             }
-            _turn = 0;
-            firsThrow = true;
-            _score = 0;
-            _pot = 50;
+            resetGame();
         }else if (_score == 36) {
             if (_turn == 0) {
                 System.out.println("Vous avez perdu");
@@ -110,27 +124,28 @@ public class ThirtySix extends AppCompatActivity {
                 System.out.println("Vous avez gagné");
                 Perso.setMoney(Perso.getMoney() + _pot);
             }
-            _turn = 0;
-            firsThrow = true;
-            _score = 0;
-            _pot = 50;
+            resetGame();
         }else {
             _turn++; _turn %= 2;
-            System.out.println("personne n'a gagné");
+            System.out.println("personne n'a gagné. tour de " + _turn);
         }
     };
 
     void opponentPlay() {
         int randNum = Perso.getSelectedRNG().getNumber(6);
         _score += (randNum+1);
+        _dice = (randNum+1);
         System.out.println("adversaire joue " + (randNum+1));
     };
 
-    void updateScreen(TextView pot, TextView turn, TextView score) {
+    void updateScreen(TextView pot, TextView turn, TextView score, TextView dice) {
+        // mettre a jour argent du jouuer
+        System.out.println("screen update");
         String p = _pot + "$";
         pot.setText(p);
         if (_turn == 0) { turn.setText(myTurn); }
         else { turn.setText(oppTurn); }
         score.setText(String.valueOf(_score));
+        dice.setText(String.valueOf(_dice));
     };
 } // end ThirtySix
